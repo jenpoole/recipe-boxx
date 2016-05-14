dc5560974f4086687e66b0fe690ee609140fefda
Jen <contact.vandepool@gmail.com>
2016-05-14 23:03:15 +0000

    edit chef profile

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -18,12 +18,23 @@ class ChefsController < ApplicationController
   
   # edit chef profile
   def edit 
+    @chef = Chef.find(params[:id])
   end
   
   def update 
+    @chef = Chef.find(params[:id])
+    
+    if @chef.update(chef_params)
+      flash[:success] = "Your profile has been updated successfully."
+      redirect_to recipes_path #change redirect to chef show page later
+    else
+      render 'edit'
+    end
   end
   
+  
   private
+  
     def chef_params
       params.require(:chef).permit(:chefname, :email, :password)
     end
