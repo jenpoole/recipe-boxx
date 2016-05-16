9643afdc68a5bdec0fe374950fe80dbd1f021fe8
Jen <contact.vandepool@gmail.com>
2016-05-16 22:54:00 +0000

    styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,6 @@ class PagesController < ApplicationController
   # if user is logged in, display recipes index
   # otherwise, display welcome page
   def home
-    # @body_id = "homepage"
     redirect_to recipes_path if logged_in?
   end
 end
